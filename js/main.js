e925fd1a1a483329a0db481fe97bf3f09f7952b6
Herin <19507887+herinlakdawala@users.noreply.github.com>
2020-05-25 18:56:50 +0530

    time span change

--- js/main.js
+++ js/main.js
@@ -80,7 +80,7 @@
 
 
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 1, 2019 12:00:00").getTime();
+var countDownDate = new Date("Dec 1, 2020 12:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
